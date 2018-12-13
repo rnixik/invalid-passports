95189f4c4cbea5b4488bba06fc6a955dc57dd60e
Roman Nix <rnix@yandex.ru>
2018-12-13 13:37:50 +0700

    Improve nginx conf

--- app/bin/benchmark.php
+++ app/bin/benchmark.php
@@ -3,19 +3,25 @@
 $invalidSeries = '7504';
 $invalidNumber = '328185';
 
-for ($i = 0; $i < 10; $i += 1) {
-    $series = 1234 - $i;
+$measurements = [];
+
+for ($i = 0; $i < 1000; $i += 1) {
+    $series = 9234 - min($i, 8000);
     $number = 123456 + $i;
     if (rand(1, 10) > 7) {
         $series = $invalidSeries;
         $number = $invalidNumber;
     }
-    $url = "http://nginx/?series={$series}&number={$number}";
+    $url = "http://nginx/index.php?series={$series}&number={$number}";
     $startTime = microtime(true);
     $response = file_get_contents($url);
     $timeInSeconds = (microtime(true) - $startTime);
     $time = number_format(round($timeInSeconds * 1000 * 10) / 10, 1, '.', '');
     echo "{$time}ms $series $number: $response" . PHP_EOL;
+    $measurements[] = $timeInSeconds;
 }
+
+echo 'Min: ' . min($measurements) . ', Max: ' . max($measurements)
+    . ', Avg: ' . array_sum($measurements) / count($measurements) . PHP_EOL;
 
 echo 'Done' . PHP_EOL;
